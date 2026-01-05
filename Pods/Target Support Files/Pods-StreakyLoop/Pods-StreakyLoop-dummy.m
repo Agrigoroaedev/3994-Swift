6ca949839b0b2c4d14b0e305dcff3f7b9659ed32
SBabchukTM <babchuk.serhii@trident-media.agency>
2026-01-05 16:08:19 +0200

    init

---
+++ Pods/Target Support Files/Pods-StreakyLoop/Pods-StreakyLoop-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StreakyLoop : NSObject
+@end
+@implementation PodsDummy_Pods_StreakyLoop
+@end
---
+++ StreakyLoop/App/StreakyLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakyLoopApp.swift
+//  StreakyLoop
+//
+//  Created by Serhii Babchuk on 05.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct StreakyLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SLLaunchView()
+        }
+    }
+}
