ed55785f54826f5f70c4ad00d3ba7c56617b8261
SBabchukTM <babchuk.serhii@trident-media.agency>
2026-01-09 13:04:01 +0200

    add grey part

--- StreakyLoop/App/StreakyLoopApp.swift
+++ StreakyLoop/App/StreakyLoopApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct StreakyLoopApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            SLLaunchView()
+            AppEntryPoint()
         }
     }
 }
